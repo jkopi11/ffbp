37981e96227ceebe640fdd679ec8c03ce4fe9995
Joseph Kopinski <joseph.kopinski@cnxcorp.com>
2016-09-26 16:53:20 -0500

    saveLineup bug fix

--- updateLineup.sql
+++ updateLineup.sql
@@ -3,9 +3,12 @@ DELIMITER //
 CREATE PROCEDURE updateLineup (IN userID INT, IN lineupID INT, IN playerID INT, IN siteName varchar(10))
 	BEGIN
 		IF EXISTS (SELECT * FROM lineup_players WHERE USER_ID=userID AND LINEUP_ID = lineupID AND SITE_P_ID = playerID AND SITE_NAME = siteName) THEN
+			INSERT INTO logs (log) VALUES ('UPDATE');
 			UPDATE lineup_players SET ACTIVE=true WHERE USER_ID=userID AND LINEUP_ID = lineupID AND SITE_P_ID = playerID AND SITE_NAME = siteName;
 		ELSE
-			INSERT INTO lineup_players (SITE_NAME,SITE_P_ID,LINEUP_ID,USER_ID) VALUES (siteName, playerID, lineupID, userID);
+			SET @usql = CONCAT('INSERT INTO lineup_players (SITE_NAME,SITE_P_ID,LINEUP_ID,USER_ID,ACTIVE) VALUES (',siteName,',',playerID,',',lineupID,',',userID,',true);');
+			INSERT INTO logs (log) VALUES (@usql);
+			INSERT INTO lineup_players (SITE_NAME,SITE_P_ID,LINEUP_ID,USER_ID,ACTIVE) VALUES (siteName, playerID, lineupID, userID, true);
 		END IF;
-	END//
+ 	END// 
 DELIMITER ;
